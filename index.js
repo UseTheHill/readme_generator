9abe4e2dfbeadd5b16d03cd1f935456d66a90e98
Kayte McDonough <kaytemcdonough@gmail.com>
2021-03-22 13:33:45 -0400

    finished typing first draft of prompt questions.

--- index.js
+++ index.js
@@ -8,6 +8,52 @@ inquirer
             message: 'What is your GitHub username?',
             name: 'username',
         },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Description of your project:',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'How can users install the necessary dependencies to use the project on their local machine?',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'What are the instruction and examples for use?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Credit for individuals, sites and any resource that made this project happen.',
+            name: 'credits',
+        },
+        {
+            type: 'list',
+            message: 'Select a license type.',
+            choices: ['MIT', 'GPLv2', 'Apache', 'Other'],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'What are the contribution guidelines?',
+            name: 'contributing',
+        },
+        {
+            type: 'input',
+            message: 'Write tests for your application and provide examples on how to run them.',
+            name: 'tests',
+        },
     ])
 
     //then
