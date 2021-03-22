379fd1abb9d0b1aa0941273b327443222512b5da
Kayte McDonough <kaytemcdonough@gmail.com>
2021-03-22 13:47:02 -0400

    added syntax for where the responses will go in the readmedoc

--- index.js
+++ index.js
@@ -59,4 +59,47 @@ inquirer
     //then
     .then((response) => {
         console.log('response: ', response)
+
+    const readmedoc = `#${response.title}
+    
+    ![GitHub](https://img.shields.io/badge/License-${response.license}-green)
+
+    
+    ## Table of Contents 
+    [Description](#description)  
+    [Installation](#installation)  
+    [Usage](#usage)  
+    [Credits](#credits)  
+    [License](#license)  
+    [Contributing](#contributing)  
+    [Tests](#tests)  
+    [Questions](#questions)  
+    
+
+    ## Description
+    ${response.description}
+
+    ## Installation
+    ${response.installation}
+
+    ## Usage
+    ${response.usage}
+
+    ## Credits
+    ${response.credits}
+
+    ## License 
+    Copyright & copy; Licensed under the ${response.license} license.
+
+    ## Contributing
+    ${response.contributing}
+
+    ## Tests
+    ${response.tests}
+
+    ## Questions
+    Conact me at ${response.email} with questions. You can submit a pull request or issue on GitHub [https://github.com/${response.username}](https://github.com/${response.username}).`;
+    
+
+    fs.writeFile('README.md', readmedoc, (err) => err ? console.error(err) : console.log('Success!'));
     });
