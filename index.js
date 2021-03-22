8d73c1a81a86b422df21ffde04b936bd4e7661fe
Kayte McDonough <kaytemcdonough@gmail.com>
2021-03-22 10:27:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: ,
+            message: ,
+            name: ,
+        }
+    ]);
+
+    //then
+    .then((response) => {
+        console.log('response: ', response)
+    });
